672fc376d31a90d40ea80131fe2a14815b2f8c07
ct_ito_hironori <ito_hironori@cyberagent.co.jp>
2023-02-28 15:34:28 +0900

    不具合修正

--- Assets/ActionSequencer/Editor/ActionSequencerSettings.cs
+++ Assets/ActionSequencer/Editor/ActionSequencerSettings.cs
@@ -23,9 +23,17 @@ namespace ActionSequencer.Editor {
         private SequenceEventTypeSetting[] _sequenceEventTypeSettings = Array.Empty<SequenceEventTypeSetting>();
 
         // SequenceEvent名 > SequenceEventSetting変換
-        private Dictionary<string, SequenceEventTypeSetting> _sequenceEventTypeSettingDict =
-            new Dictionary<string, SequenceEventTypeSetting>();
-        public IReadOnlyDictionary<string, SequenceEventTypeSetting> SequenceEventTypeSettings => _sequenceEventTypeSettingDict;
+        private Dictionary<string, SequenceEventTypeSetting> _sequenceEventTypeSettingDict;
+        public IReadOnlyDictionary<string, SequenceEventTypeSetting> SequenceEventTypeSettings {
+            get {
+                if (_sequenceEventTypeSettingDict == null) {
+                    _sequenceEventTypeSettingDict = new Dictionary<string, SequenceEventTypeSetting>();
+                    RefreshSettingDict();
+                }
+
+                return _sequenceEventTypeSettingDict;
+            }
+        }
 
         /// <summary>
         /// 保存処理
@@ -35,14 +43,32 @@ namespace ActionSequencer.Editor {
         }
 
         /// <summary>
-        /// 値変更時通知
+        /// 設定ファイル検索用辞書の更新
         /// </summary>
-        private void OnValidate() {
+        private void RefreshSettingDict() {
             _sequenceEventTypeSettingDict.Clear();
-            
+
             foreach (var setting in _sequenceEventTypeSettings) {
                 _sequenceEventTypeSettingDict[setting.fullName] = setting;
             }
+        }
+
+        /// <summary>
+        /// アクティブ時処理
+        /// </summary>
+        private void OnEnable() {
+            RefreshSettingDict();
+        }
+
+        /// <summary>
+        /// 値変更時通知
+        /// </summary>
+        private void OnValidate() {
+            if (_sequenceEventTypeSettingDict == null) {
+                _sequenceEventTypeSettingDict = new Dictionary<string, SequenceEventTypeSetting>();
+            }
+
+            RefreshSettingDict();
         }
     }
 }
