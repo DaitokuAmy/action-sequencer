52eeeb3c10a735b2a73e17e938bdf500209a8d1f
ito_hironori <daitoku1988@gmail.com>
2023-03-18 15:18:39 +0900

    アニメーションPreview用の機能追加

--- Packages/com.daitokuamy.actionsequencer/Editor/VisualElements/AnimationClipView.cs
+++ Packages/com.daitokuamy.actionsequencer/Editor/VisualElements/AnimationClipView.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Object = UnityEngine.Object;
 
 namespace ActionSequencer.Editor.VisualElements {
     /// <summary>
@@ -10,25 +13,92 @@ namespace ActionSequencer.Editor.VisualElements {
         public new class UxmlFactory : UxmlFactory<AnimationClipView, UxmlTraits> {
         }
 
+        /// <summary>
+        /// AnimationClipのEditor操作用
+        /// </summary>
+        private class AnimationClipEditor : IDisposable {
+            private UnityEditor.Editor _editor;
+            private object _timeControl;
+            private FieldInfo _currentTimeFieldInfo;
+
+            public Object Target => _editor != null ? _editor.target : null;
+            public UnityEditor.Editor Editor => _editor;
+            public float CurrentTime => (_timeControl != null && _currentTimeFieldInfo != null)
+                ? (float)_currentTimeFieldInfo.GetValue(_timeControl)
+                : 0.0f;
+
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            public AnimationClipEditor(AnimationClip clip) {
+                _editor = UnityEditor.Editor.CreateEditor(clip);
+            }
+
+            /// <summary>
+            /// フィールドの初期化
+            /// </summary>
+            public void SetupFields() {
+                if (_editor == null) {
+                    return;
+                }
+
+                // 制御用のPropertyを抽出
+                var avatarPreviewFieldInfo = _editor.GetType().GetField("m_AvatarPreview",
+                    BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+                var avatarPreview = avatarPreviewFieldInfo?.GetValue(_editor);
+                if (avatarPreview != null) {
+                    var timeControlFieldInfo = avatarPreview.GetType().GetField("timeControl",
+                        BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+                    _timeControl = timeControlFieldInfo?.GetValue(avatarPreview);
+                    if (_timeControl != null) {
+                        _currentTimeFieldInfo = _timeControl.GetType().GetField("currentTime");
+                    }
+                }
+            }
+
+            /// <summary>
+            /// 廃棄時処理
+            /// </summary>
+            public void Dispose() {
+                if (_editor == null) {
+                    Object.DestroyImmediate(_editor);
+                }
+
+                _currentTimeFieldInfo = null;
+                _timeControl = null;
+                _editor = null;
+            }
+        }
+
         private IMGUIContainer _container;
-        private UnityEditor.Editor _animationClipEditor;
-        private bool _editorInitialized = false;
+        private AnimationClipEditor _animationClipEditor;
+        private bool _initializedField;
         private GUIStyle _previewStyle;
+
+        // Preview使用中か
+        public bool IsValid => _animationClipEditor?.Target != null;
+        // 現在の再生時間
+        public float CurrentTime => _animationClipEditor?.CurrentTime ?? 0.0f;
+        // 変更通知
+        public event Action<AnimationClip> OnChangedClipEvent; 
 
         /// <summary>
         /// コンストラクタ
         /// </summary>
         public AnimationClipView() {
-            RegisterCallback<DetachFromPanelEvent>(evt => { ClearTarget(); });
+            CreateContainer();
         }
 
         /// <summary>
         /// ターゲットの設定
         /// </summary>
         public void SetTarget(AnimationClip target) {
+            if (_animationClipEditor != null && _animationClipEditor.Target == target) {
+                return;
+            }
+            
             ClearTarget();
             CreateEditor(target);
-            CreateContainer();
         }
 
         /// <summary>
@@ -47,7 +117,17 @@ namespace ActionSequencer.Editor.VisualElements {
             }
 
             _container = new IMGUIContainer(() => {
-                if (_animationClipEditor == null) {
+                // Clipの指定
+                using (var changeScope = new EditorGUI.ChangeCheckScope()) {
+                    var clip = _animationClipEditor?.Target as AnimationClip;
+                    clip = EditorGUILayout.ObjectField(clip, typeof(AnimationClip), false) as AnimationClip;
+                    if (changeScope.changed) {
+                        SetTarget(clip);
+                        return;
+                    }
+                }
+                
+                if (_animationClipEditor?.Editor == null) {
                     return;
                 }
 
@@ -55,24 +135,42 @@ namespace ActionSequencer.Editor.VisualElements {
                 if (_previewStyle == null) {
                     _previewStyle = new GUIStyle {
                         normal = {
-                            background = EditorGUIUtility.whiteTexture
+                            background = Texture2D.grayTexture
                         }
                     };
                 }
 
-                // 初期化をInspectorで行っているので一回呼ぶ
-                if (!_editorInitialized) {
-                    _animationClipEditor.OnInspectorGUI();
-                    _editorInitialized = true;
+                // 基本操作を回している部分
+                _animationClipEditor.Editor.OnInspectorGUI();
+                if (!_initializedField) {
+                    _animationClipEditor.SetupFields();
+                    _initializedField = true;
                 }
                 
+                // 描画を上書きする
+                var rect = contentContainer.layout;
+                
                 // Preview描画
-                if (_animationClipEditor.HasPreviewGUI()) {
+                if (_animationClipEditor.Editor.HasPreviewGUI()) {
+                    rect.yMin += EditorGUIUtility.singleLineHeight;
+                    GUILayout.BeginArea(rect);
+                    
                     using (new GUILayout.HorizontalScope()) {
-                        _animationClipEditor.OnPreviewSettings();
+                        var leftRect = rect;
+                        leftRect.height = EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                        leftRect.y = 0.0f;
+                        leftRect.xMax -= 150;
+                        EditorGUI.DrawRect(leftRect, new Color(0.15f, 0.15f, 0.15f));
+                        EditorGUILayout.Space(leftRect.width - 10);
+                        _animationClipEditor.Editor.OnPreviewSettings();
                     }
-                    _animationClipEditor.OnInteractivePreviewGUI(contentContainer.layout, _previewStyle);
+                
+                    GUILayout.EndArea();
+                    rect.yMin += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+                    _animationClipEditor.Editor.OnInteractivePreviewGUI(rect, _previewStyle);
                 }
+            
+                MarkDirtyRepaint();
             });
             Add(_container);
         }
@@ -80,19 +178,21 @@ namespace ActionSequencer.Editor.VisualElements {
         /// <summary>
         /// AnimationClip用のEditorを生成
         /// </summary>
-        private void CreateEditor(AnimationClip clip) {
-            if (_animationClipEditor != null && _animationClipEditor.target == clip) {
+        private void CreateEditor(AnimationClip clip, bool invokeEvent = true) {
+            if (_animationClipEditor != null && _animationClipEditor.Target == clip) {
                 return;
             }
 
             if (_animationClipEditor != null) {
-                Object.DestroyImmediate(_animationClipEditor);
+                _animationClipEditor.Dispose();
                 _animationClipEditor = null;
             }
 
-            if (clip != null) {
-                _animationClipEditor = UnityEditor.Editor.CreateEditor(clip);
-                _editorInitialized = false;
+            _animationClipEditor = new AnimationClipEditor(clip);
+            _initializedField = false;
+
+            if (invokeEvent) {
+                OnChangedClipEvent?.Invoke(clip);
             }
         }
     }
